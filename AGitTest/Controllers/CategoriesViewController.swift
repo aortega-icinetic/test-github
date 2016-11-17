bcd7bea335842a94f6a3f75c10700373b64f2eec
IBM Mobile UI Builder (using 'aortega-icinetic' account) <IBM Mobile UI Builder (using 'aortega-icinetic' account)>
2016-11-17 10:04:02 +0000

    dev-8

---
+++ AGitTest/Controllers/CategoriesViewController.swift
@@ -0,0 +1,86 @@
+//
+//  CategoriesViewController.swift
+//  AGitTest
+//
+//  This App has been generated using IBM Mobile UI Builder
+//
+
+import UIKit
+
+class CategoriesViewController: TableViewController<MenuItem>, UITableViewDataSource, UITableViewDelegate {
+    
+    typealias Cell = BasicImageCell
+
+    
+	override init() {
+        super.init()
+    }
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        AnalyticsManager.sharedInstance?.analytics?.logPage("Categories")
+        
+		navigationItem.title = NSLocalizedString("Categories", comment: "")
+		
+        items = [
+			MenuItem(text: "test", action: NavigationAction<TestViewController>(rootViewController: self), image: "DefaultMenuIcon.png"),
+			MenuItem(text: "1 a", action: NavigationAction<MenuItem0ViewController>(rootViewController: self), image: "DefaultMenuIcon.png"),
+			MenuItem(text: "MyCustom", action: NavigationAction<MyCustomViewController>(rootViewController: self), image: "DefaultMenuIcon.png"),
+		
+        ]
+        
+		tableView.dataSource = self
+        tableView.delegate = self
+        tableView.registerClass(Cell.self, forCellReuseIdentifier: Cell.identifier)
+    
+
+        for behavior in self.behaviors {
+            behavior.load()
+        }
+		
+		updateViewConstraints()
+    }
+  
+    //MARK: - <UITableViewDataSource>
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return items.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(Cell.identifier, forIndexPath: indexPath) as! Cell
+        
+        let item = items[indexPath.row]
+
+        cell.titleLabel.text = item.text
+		cell.photoImageView.loadImage(item.image)
+        cell.userInteractionEnabled = false
+        cell.accessoryView = nil
+        cell.accessoryType = .None
+        if let action = item.action where action.canBeExecuted() {
+            
+            cell.userInteractionEnabled = true
+            // You can customize the accessory icon ...
+            let icon = UIImage(named: Images.arrow)?.imageWithRenderingMode(.AlwaysTemplate)
+            let accessoryImageView = UIImageView(image: icon)
+            accessoryImageView.tintColor = Style.sharedInstance.foregroundColor
+            cell.accessoryView = accessoryImageView
+        }
+        
+        return cell
+    }
+    
+    //MARK: - <UITableViewDelegate>
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        let item = items[indexPath.row]
+        item.action?.execute()
+    }
+}
+
---
+++ AGitTest/Models/GymsScreen1DSItem.swift
@@ -0,0 +1,73 @@
+//
+//  GymsScreen1DSItem.swift
+//  AGitTest
+//
+//  This App has been generated using IBM Mobile UI Builder
+//
+
+import Foundation
+
+enum GymsScreen1DSItemMapping {
+	static let name = "name"
+	static let description = "description"
+	static let picture = "picture"
+	static let phone = "phone"
+	static let location = "location"
+	static let address = "address"
+	static let rating = "rating"
+	static let id = "id"
+}
+
+public class GymsScreen1DSItem : Item {
+	
+	var name : String?
+	var description : String?
+	var picture : String?
+	var phone : String?
+	var location : GeoPoint?
+	var address : String?
+	var rating : String?
+	var id : String?
+	
+	// MAR: - <Item>
+
+	public var identifier: AnyObject? {
+		guard let identifier = id else {
+			return ""
+		}
+		return identifier
+	}
+	
+	required public init?(dictionary: NSDictionary?) {
+    
+        retrieve(dictionary)
+    }
+	
+	public func retrieve(dictionary: NSDictionary?) {
+     
+        guard let dic = dictionary else {
+            return
+        }
+        
+ 		name = dic[GymsScreen1DSItemMapping.name] as? String
+		
+		description = dic[GymsScreen1DSItemMapping.description] as? String
+		
+		picture = dic[GymsScreen1DSItemMapping.picture] as? String
+		
+		phone = dic[GymsScreen1DSItemMapping.phone] as? String
+		
+			
+		location = GeoPoint(dictionary: dic[GymsScreen1DSItemMapping.location] as? NSDictionary)
+		
+		address = dic[GymsScreen1DSItemMapping.address] as? String
+		
+		rating = dic[GymsScreen1DSItemMapping.rating] as? String
+		
+		id = dic[GymsScreen1DSItemMapping.id] as? String
+		
+	   
+    }
+}
+	
+
